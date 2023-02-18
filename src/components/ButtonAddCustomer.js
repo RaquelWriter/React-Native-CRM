c8137d1855b3bf36ba6188587e6cb6d68660b392
RaquelWriter <raquel@mundofeliz.com>
2023-02-18 17:29:08 +0100

    Adding crypto id to the button to send a new id to the form

--- src/components/ButtonAddCustomer.js
+++ src/components/ButtonAddCustomer.js
@@ -3,12 +3,13 @@ import { Text, TouchableOpacity } from 'react-native';
 import stylesFn from './styles';
 
 const ButtonAddCustomer = () => {
+  const newId = crypto.randomUUID();
   const styles = stylesFn();
   const { navigate } = useNavigation();
   return (
     <TouchableOpacity
       style={Object.assign({}, styles.buttons, styles.buttonCreate)}
-      onPress={() => navigate('New')}
+      onPress={() => navigate('New', { id: newId })}
     >
       <Text style={styles.textButton}>Add customer</Text>
     </TouchableOpacity>
