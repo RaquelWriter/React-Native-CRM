8a62c3527814496ed5e2a00fc1a41bbd900fbfb7
RaquelWriter <raquel@mundofeliz.com>
2023-02-21 17:27:31 +0100

    fix: fix line 25, adding customer to previous ones

--- src/features/Customers/sagas/new.js
+++ src/features/Customers/sagas/new.js
@@ -2,30 +2,34 @@ import { all, put, select, takeLatest, delay } from 'redux-saga/effects';
 import * as actions from '../reducers';
 import { set } from '../../../utilities/async_storage';
 
-export function* watchcreateCustomer() {
+export function* watchCreateCustomer() {
   yield takeLatest(actions.createCustomer.toString(), takeCreateCustomer);
 }
 
-export function* takeCreateCustomer() {
-  console.log('Starting fetch request to API');
+export function* takeCreateCustomer(action) {
   try {
-    const fields = yield select((state) => state.customer.form.fields);
+    const fields = yield select((state) => state.customers.form.fields);
     const customers = yield select((state) => state.customers.customers);
-
     const customer = {
       ...fields,
     };
-
+    console.log(
+      'Sending a NEW CUSTOMER TO THE DB, fields: ',
+      customer,
+      ' CUSTOMERS: ',
+      customers
+    );
     // pretend call to API
     yield delay(500);
 
-    const result = [...customers, customer];
+    const result = [customer, ...customers];
 
-    // Saving to th async_storage also:
+    // Saving to the device memory with async storage
     yield set('CUSTOMERS_KEY', result);
 
     yield put(actions.createCustomerResult(result));
   } catch (error) {
     yield put(actions.createCustomerError(error.toString()));
+    console.log('ERROR!!!!!!!!: ', error);
   }
 }
