46ed4e58d6db77c1be271165550cc75660626f51
RaquelWriter <raquel@mundofeliz.com>
2023-02-21 17:33:01 +0100

    style: removing comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -104,51 +104,12 @@ const Form = ({ disabled = false }) => {
         onValueChange={handleSwitch}
         value={isActive}
       ></Switch>
-      {/* <TextInput
-        key={'region'}
-        placeholder={region}
-        value={region || ''}
-        style={styles.form}
-        onChangeText={(v) => setFormField('region', v)}
-      /> */}
       <TouchableOpacity
         style={Object.assign({}, styles.buttons, styles.buttonCreate)}
         onPress={handleSubmit}
       >
         <Text style={styles.textButton}>Submit</Text>
       </TouchableOpacity>
-
-      {/* <TextInput
-        key={'common_name'}
-        placeholder='Common Name'
-        value={common_name || ''}
-        style={{
-          borderWidth: 1,
-          borderColor: 'black',
-          borderRadius: 4,
-          padding: 15,
-        }}
-        onChangeText={(v) => setFormField('common_name', v)}
-      />
-
-      <View style={{ height: 15, width: '100%' }}></View>
-
-      <TextInput
-        key={'scientific_name'}
-        placeholder='Scientific Name'
-        value={scientific_name || ''}
-        style={{
-          borderWidth: 1,
-          borderColor: 'black',
-          borderRadius: 4,
-          padding: 15,
-        }}
-        onChangeText={(v) => setFormField('scientific_name', v)}
-      />
-
-      <TouchableOpacity onPress={onSubmit}>
-        <Text>Submit</Text>
-      </TouchableOpacity> */}
     </View>
   );
 };
